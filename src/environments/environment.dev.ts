588b4d631a513b1cb869ca7236b23d5a80439e2d
raghurichards <raghurichards@gmail.com>
2022-01-27 00:05:33 +0400

    initial commit

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: false,
   BASE_URL: 'http://localhost:3000/',
+  // BASE_URL: 'https://loanmanager-d860b.web.app:3000/',
+
   CUSTOMER_BASE_URL: 'http://localhost:3000/customers/',
+  //CUSTOMER_BASE_URL: 'https://loanmanager-d860b.web.app:3000/customers/',
+
   CUSTOMER: {
     POST_CUSTOMER: 'add',
     GET_ALL_CUSTOMERS: 'list',
